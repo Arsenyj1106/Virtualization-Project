012a9d49bcf5497914ca3b9b2592b5221dbc0345
Lench228 <leonidgneusev@gmail.com>
2025-05-19 09:48:50 +0500

    feat: add postgres

---
+++ terraform/generate_fqdn.tf
@@ -0,0 +1,6 @@
+resource "local_file" "env_file" {
+  content = <<-EOF
+    PG_LINK=postgres://${var.postgresql_user}:${var.postgresql_password}@${yandex_mdb_postgresql_cluster.todo-postgres-cluster.host[0].fqdn}:6432/${var.postgresql_db}
+  EOF
+  filename = "../.env"
+}
